2285c16741a3f79652324d9b3e15760367146200
Ahn Kwang Hyun <ahnkwanghyun@Ahns-MacBook-Air.local>
2025-04-19 23:24:13 +0900

    fea: jemter 환경 설정 완료 resolve #7

--- src/main/java/org/faddy/community_feed/post/repository/post_queue/UserQueueRedisRepositoryImpl.java
+++ src/main/java/org/faddy/community_feed/post/repository/post_queue/UserQueueRedisRepositoryImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.faddy.community_feed.post.repository.entity.post.PostEntity;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 @Repository
